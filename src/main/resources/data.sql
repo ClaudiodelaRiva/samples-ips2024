ca15b2bf061a105121be0cc80e2753fe0bd80aee
Claudio de la Riva <Claudio de la Riva>
2024-10-09 14:33:33 +0200

    Modificación esquema y carga de datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,20 @@
 --Datos para carga inicial de la base de datos
 
 --Para giis.demo.tkrun:
+delete from inscripcion;
+delete from atleta
 delete from carreras;
 insert into carreras(id,inicio,fin,fecha,descr) values 
 	(100,'2016-10-05','2016-10-25','2016-11-09','finalizada'),
 	(101,'2016-10-05','2016-10-25','2016-11-10','en fase 3'),
 	(102,'2016-11-05','2016-11-09','2016-11-20','en fase 2'),
 	(103,'2016-11-10','2016-11-15','2016-11-21','en fase 1'),
-	(104,'2016-11-11','2016-11-15','2016-11-22','antes inscripcion');
+	(104,'2016-11-11','2016-11-15','2016-11-22','antes inscripcion')
+        (105,'2024-08-01','2024-11-01','2024-10-06','en fase 1')
+;
+insert into atletas (id,nombre,fechaNacimiento) values
+	(100, A1, 1992-01-01),
+	(101, A2, 2010-01-01),
+	(102, A3, 2000-01-01);
+insert into inscripcion (id,fechaInscripcion,idCarrera,idAtleta) values
+	(101, I1, 2016-10-06, 100, 100);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,15 @@
 --(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
 
 --Para giis.demo.tkrun:
+
+drop table Inscripcion;
 drop table Carreras;
+drop table Atleta:
+
 create table Carreras (id int primary key not null, inicio date not null, fin date not null, fecha date not null, descr varchar(32), check(inicio<=fin), check(fin<fecha));
+create table Atleta (id int primary key not null, nombre varchar (50) not null, fechaNacimiento date not null);
+create table Inscripcion (id int primary key not null, fechaInscripcion date not null, idCarrera int,  idAtleta int, 
+                          foreign key (idCarrera) references Carreras(id),
+                          foreign key (idAtleta) references Atleta(id)
+                          );
 
